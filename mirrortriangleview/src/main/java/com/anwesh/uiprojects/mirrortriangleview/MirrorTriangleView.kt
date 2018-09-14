aad50174c035ddc7a95e3dd3b7a06f9cdf172939
Anwesh43 <anweshthecool0@gmail.com>
2018-09-15 03:48:33 +0530

    adding libraries and defining nodes number

--- mirrortriangleview/src/main/java/com/anwesh/uiprojects/mirrortriangleview/MirrorTriangleView.kt
+++ mirrortriangleview/src/main/java/com/anwesh/uiprojects/mirrortriangleview/MirrorTriangleView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.mirrortriangleview
 /**
  * Created by anweshmishra on 15/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Path
+
+val nodes : Int = 5
